9cd78185d707ee56432003c3b5fe4fef713c8c88
Belen <bloleo@dc.uba.ar>
2026-01-29 21:54:03 -0300

    test desploy corrigiendo vite config y deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/',
+  base: './', // Importante para GitHub Pages
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  },
   plugins: [
     tailwindcss(),
   ],
